399a650a972cb9c286d48917a3b9e4894508e73f
BeginArtem <lartem.1205@gmail.com>
2022-06-08 14:31:24 +0300

    add: init general structure

--- target/wasm32-unknown-unknown/release/deps/near_sys-cfab32a3e7ec1d2b.d
+++
@@ -1,7 +0,0 @@
-/home/user/CLionProjects/contracts/target/wasm32-unknown-unknown/release/deps/near_sys-cfab32a3e7ec1d2b.rmeta: /home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/near-sys-0.2.0/src/lib.rs
-
-/home/user/CLionProjects/contracts/target/wasm32-unknown-unknown/release/deps/libnear_sys-cfab32a3e7ec1d2b.rlib: /home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/near-sys-0.2.0/src/lib.rs
-
-/home/user/CLionProjects/contracts/target/wasm32-unknown-unknown/release/deps/near_sys-cfab32a3e7ec1d2b.d: /home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/near-sys-0.2.0/src/lib.rs
-
-/home/user/.cargo/registry/src/github.com-1ecc6299db9ec823/near-sys-0.2.0/src/lib.rs:
