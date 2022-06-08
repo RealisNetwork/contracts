1caed826076057bd16def09b9fa0531559d376c9
gleb protasov <protasovgleb290@gmail.com>
2022-06-08 13:05:07 +0300

    mint nft func + test

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,32 @@
-mod utils;
-mod account;
-mod public_api;
-mod backend_api;
-mod types;
+#![feature(gen_future)]
+
+use near_sdk::AccountId;
+use near_sdk::borsh::{BorshDeserialize, BorshSerialize};
+use near_sdk::collections::{LookupMap, LookupSet};
+use near_sdk::{near_bindgen};
+use near_sdk::PanicOnDefault;
+use near_sdk::serde::{Serialize,Deserialize};
+
+// mod utils;
+// mod account;
+// mod public_api;
+// mod backend_api;
+// mod types;
 mod owner;
-
-use near_sdk::borsh::{BorshSerialize, BorshDeserialize};
-
+#[derive(BorshDeserialize, BorshSerialize,Serialize,Deserialize)]
 pub enum State {
     Paused,
     Running,
 }
 
 #[near_bindgen]
-#[derive(BorshSerialize, BorshDeserialize, PanicOnDefault)]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
     // AccountId -> Account
     // NftId -> Nft
-    // owner_id: AccountId
+    owner_id: AccountId,
+    nft_ids: LookupSet<String>,
+    users_nft: LookupMap<AccountId, LookupSet<String>>,
     // backend_id: AccountId
     // beneficiary_id: AccountId
     // fee: ???
--- src/owner.rs
+++ src/owner.rs
@@ -1,11 +1,32 @@
-use near_sdk::{AccountId, Timestamp};
+use near_sdk::{AccountId, env, Timestamp};
 use near_sdk::json_types::U128;
+
 use crate::*;
 
 #[near_bindgen]
 impl Contract {
-    pub fn mint(&mut self, recipient_id: AccountId, nft_metadata: String) {
-        todo!()
+
+    pub fn mint(&mut self, recipient_id: AccountId, nft_metadata: String) -> String {
+        self.only_owner();
+
+        let nft_id = env::sha256(nft_metadata.as_bytes());
+        let nft_id = &hex::encode(nft_id);
+
+        assert!(self.nft_ids.insert(nft_id), "Nft already exist");
+
+        match self.users_nft.get(&recipient_id) {
+            Some(mut set_of_nft) =>
+                assert!(set_of_nft.insert(nft_id), "User already have this nft "),
+            None => {
+                let mut new_set = LookupSet::new(env::sha256(recipient_id.as_bytes()));
+                new_set.insert(nft_id);
+                self.users_nft.insert(&recipient_id, &new_set);
+            }
+        }
+        nft_id.to_string()
+    }
+    fn only_owner(&mut self) {
+        assert_eq!(env::predecessor_account_id(), self.owner_id.clone(), "Only owner can mint nft");
     }
 
     pub fn change_state(&mut self, state: State) {
@@ -22,5 +43,59 @@ impl Contract {
 
     pub fn create_account() {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use near_sdk::{testing_env, VMContext};
+    use near_sdk::test_utils::VMContextBuilder;
+
+    use super::*;
+
+    pub fn get_context(caller_id: String) -> VMContext {
+        VMContextBuilder::new()
+            .predecessor_account_id(AccountId::new_unchecked(caller_id))
+            .is_view(false)
+            .build()
+    }
+
+
+    pub fn get_contract() -> Contract {
+        Contract {
+            owner_id: "owner".to_string().parse().unwrap(),
+            nft_ids: LookupSet::new(b"s"),
+            users_nft: LookupMap::new(b"m"),
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn mint_nft_test_panic() {
+        let mut contract = get_contract();
+        let context = get_context("not an owner".to_string());
+        testing_env!(context);
+        let res = contract.mint(AccountId::new_unchecked("user_id".to_string()), "some_metadata".to_string());
+    }
+
+    #[test]
+    fn mint_nft_test() {
+        let mut contract = get_contract();
+        let context = get_context("owner".to_string());
+        testing_env!(context);
+        let res = contract.mint(AccountId::new_unchecked("user_id".to_string()), "some_metadata".to_string());
+        println!("{}", res);
+        assert!(contract.nft_ids.contains(&res));
+        assert!(contract.users_nft.get(&AccountId::new_unchecked("user_id".to_string())).is_some())
+    }
+
+    #[test]
+    #[should_panic]
+    fn the_same_meta_data_panic() {
+        let mut contract = get_contract();
+        let context = get_context("owner".to_string());
+        testing_env!(context);
+        contract.mint(AccountId::new_unchecked("user_id".to_string()), "some_metadata".to_string());
+        contract.mint(AccountId::new_unchecked("user_id".to_string()), "some_metadata".to_string());
     }
 }
