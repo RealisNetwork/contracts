bd1efbb7992311b47d7158c15adf894b3e4ad641
BeginArtem <lartem.1205@gmail.com>
2022-06-10 14:53:34 +0300

    add: assert_running * assert_backend

--- src/backend_api.rs
+++ src/backend_api.rs
@@ -1,12 +1,12 @@
 use crate::{types::NftId, *};
-use near_sdk::{json_types::U128, require, near_bindgen, AccountId};
+use near_sdk::{json_types::U128, near_bindgen, AccountId};
 
 #[near_bindgen]
 impl Contract {
     #[allow(unused_variables)]
     pub fn backend_transfer(&mut self, recipient_id: AccountId, amount: U128) -> U128 {
-        require!(self.state == State::Running, "Contract is paused");
-        require!(env::signer_account_id() == self.backend_id, "Not allowed");
+        self.assert_running();
+        self.assert_backend();
         let sender_id = self.resolve_account(env::signer_account_pk());
         self.internal_transfer(sender_id, recipient_id, amount.0)
             .into()
@@ -14,26 +14,36 @@ impl Contract {
 
     #[allow(unused_variables)]
     pub fn backend_burn(&mut self, nft_id: NftId) {
+        self.assert_running();
+        self.assert_backend();
         todo!()
     }
 
     #[allow(unused_variables)]
     pub fn backend_transfer_nft(&mut self, recipient_id: AccountId, nft_id: NftId) {
+        self.assert_running();
+        self.assert_backend();
         todo!()
     }
 
     #[allow(unused_variables)]
     pub fn backend_sell_nft(&mut self, nft_id: NftId, price: U128) {
+        self.assert_running();
+        self.assert_backend();
         todo!()
     }
 
     #[allow(unused_variables)]
     pub fn backend_buy_nft(&mut self, nft_id: NftId) -> U128 {
+        self.assert_running();
+        self.assert_backend();
         todo!()
     }
 
     #[allow(unused_variables)]
     pub fn backend_change_price(&mut self, nft_id: NftId, price: U128) {
+        self.assert_running();
+        self.assert_backend();
         todo!()
     }
 
--- src/public_api.rs
+++ src/public_api.rs
@@ -1,32 +1,37 @@
 use crate::{types::NftId, *};
-use near_sdk::{json_types::U128, require, AccountId};
+use near_sdk::{json_types::U128, AccountId};
 
 #[near_bindgen]
 impl Contract {
     pub fn transfer(&mut self, recipient_id: AccountId, amount: U128) -> U128 {
-        require!(self.state == State::Running, "Contract is paused");
+        self.assert_running();
         let sender_id = env::signer_account_id();
         self.internal_transfer(sender_id, recipient_id, amount.0)
             .into()
     }
 
     pub fn burn(&mut self, _nft_id: NftId) {
+        self.assert_running();
         todo!()
     }
 
     pub fn transfer_nft(&mut self, _recipient_id: AccountId, _nft_id: NftId) {
+        self.assert_running();
         todo!()
     }
 
     pub fn sell_nft(&mut self, _nft_id: NftId, _price: U128) {
+        self.assert_running();
         todo!()
     }
 
     pub fn buy_nft(&mut self, _nft_id: NftId) -> U128 {
+        self.assert_running();
         todo!()
     }
 
     pub fn change_price(&mut self, _nft_id: NftId, _price: U128) {
+        self.assert_running();
         todo!()
     }
 
