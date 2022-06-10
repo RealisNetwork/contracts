5690b198103f5c5a044cac165ce39d711d89c2bc
BeginArtem <lartem.1205@gmail.com>
2022-06-10 15:16:41 +0300

    add: add tests

--- src/backend_api.rs
+++ src/backend_api.rs
@@ -50,3 +50,140 @@ impl Contract {
     // TODO: delegate nft
     // Discuss general structure of delegation
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::tests_utils::*;
+
+    #[test]
+    #[should_panic = "Contract is paused"]
+    fn backend_transfer_assert_running() {
+        let (mut contract, _context) = init_test_env(None, None, None);
+
+        contract.state = State::Paused;
+        contract.backend_transfer(accounts(1), U128(100));
+    }
+
+    #[test]
+    #[should_panic = "Not allowed"]
+    fn backend_transfer_assert_backend() {
+        let (mut contract, mut context) = init_test_env(None, None, Some(accounts(1)));
+
+        testing_env!(
+            context
+                .predecessor_account_id(accounts(2))
+                .build()
+        );
+        contract.backend_transfer(accounts(1), U128(100));
+    }
+
+    #[test]
+    #[should_panic = "Contract is paused"]
+    fn backend_burn_assert_running() {
+        let (mut contract, _context) = init_test_env(None, None, None);
+
+        contract.state = State::Paused;
+        contract.backend_burn(1);
+    }
+
+    #[test]
+    #[should_panic = "Not allowed"]
+    fn backend_burn_assert_backend() {
+        let (mut contract, mut context) = init_test_env(None, None, Some(accounts(1)));
+
+        testing_env!(
+            context
+                .predecessor_account_id(accounts(2))
+                .build()
+        );
+        contract.backend_burn(1);
+    }
+
+    #[test]
+    #[should_panic = "Contract is paused"]
+    fn backend_sell_nft_assert_running() {
+        let (mut contract, _context) = init_test_env(None, None, None);
+
+        contract.state = State::Paused;
+        contract.backend_sell_nft(1, U128(100));
+    }
+
+    #[test]
+    #[should_panic = "Not allowed"]
+    fn backend_sell_nft_assert_backend() {
+        let (mut contract, mut context) = init_test_env(None, None, Some(accounts(1)));
+
+        testing_env!(
+            context
+                .predecessor_account_id(accounts(2))
+                .build()
+        );
+        contract.backend_sell_nft(1, U128(100));
+    }
+
+    #[test]
+    #[should_panic = "Contract is paused"]
+    fn backend_change_price_assert_running() {
+        let (mut contract, _context) = init_test_env(None, None, None);
+
+        contract.state = State::Paused;
+        contract.backend_change_price(1, U128(100));
+    }
+
+    #[test]
+    #[should_panic = "Not allowed"]
+    fn backend_change_price_assert_backend() {
+        let (mut contract, mut context) = init_test_env(None, None, Some(accounts(1)));
+
+        testing_env!(
+            context
+                .predecessor_account_id(accounts(2))
+                .build()
+        );
+        contract.backend_change_price(1, U128(100));
+    }
+
+    #[test]
+    #[should_panic = "Contract is paused"]
+    fn backend_transfer_nft_assert_running() {
+        let (mut contract, _context) = init_test_env(None, None, None);
+
+        contract.state = State::Paused;
+        contract.backend_transfer_nft(accounts(1), 100);
+    }
+
+    #[test]
+    #[should_panic = "Not allowed"]
+    fn backend_transfer_nft_assert_backend() {
+        let (mut contract, mut context) = init_test_env(None, None, Some(accounts(1)));
+
+        testing_env!(
+            context
+                .predecessor_account_id(accounts(2))
+                .build()
+        );
+        contract.backend_transfer_nft(accounts(1), 1);
+    }
+
+    #[test]
+    #[should_panic = "Contract is paused"]
+    fn backend_buy_nft_assert_running() {
+        let (mut contract, _context) = init_test_env(None, None, None);
+
+        contract.state = State::Paused;
+        contract.backend_buy_nft(1);
+    }
+
+    #[test]
+    #[should_panic = "Not allowed"]
+    fn backend_buy_nft_assert_backend() {
+        let (mut contract, mut context) = init_test_env(None, None, Some(accounts(1)));
+
+        testing_env!(
+            context
+                .predecessor_account_id(accounts(2))
+                .build()
+        );
+        contract.backend_buy_nft(100);
+    }
+}
