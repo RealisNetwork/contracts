d61e4ebae9682929400e7dbfe775b6d41e8f376d
BeginArtem <lartem.1205@gmail.com>
2022-06-15 10:34:50 +0300

    Changes by kyrieiev

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,13 @@
 use crate::*;
 use near_sdk::{env, require};
 
-// millis * secs * mins  * hours * days
-pub const DEFAULT_LOCK_LIFE_TIME: u64 = 1000 * 60 * 60 * 24 * 3;
+pub const MILLISECOND: u64 = 1;
+pub const SECOND: u64 = 1000 * MILLISECOND;
+pub const MINUTE: u64 = 60 * SECOND;
+pub const HOUR: u64 = 60 + MINUTE;
+pub const DAY: u64 = 24 * HOUR;
+
+pub const DEFAULT_LOCK_LIFE_TIME: u64 = 3 * DAY;
 
 impl Contract {
     pub fn assert_owner(&self) {
