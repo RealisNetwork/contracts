57fa7c23a33e800ae27f42fa348289fdef048ba0
HolyGrease <kireevdanil2000@gmail.com>
2022-06-17 18:22:23 +0300

    impl tests util staff

--- tests/utils.rs
+++ tests/utils.rs
@@ -1,5 +1,9 @@
-use near_sdk::{json_types::U128, serde::Serialize, AccountId};
-pub use workspaces::{network::Testnet, Account, Contract, Worker};
+use near_sdk::{json_types::U128, serde::Serialize, serde_json};
+use workspaces::network::DevAccountDeployer;
+pub use workspaces::{network::Testnet, Account, AccountId, Contract, Worker};
+
+pub const WASM_FILE: &str = "./target/wasm32-unknown-unknown/release/realis_near.wasm";
+pub const ONE_LIS: u128 = 1_000_000_000_000;
 
 pub fn get_alice() -> Account {
     Account::from_file("./tests/res/realis_alice.testnet.json")
@@ -23,19 +27,45 @@ pub struct TestingEnvBuilder {
     total_supply: U128,
     constant_fee: U128,
     percent_fee: u8,
+    owner_id: AccountId,
     beneficiary_id: AccountId,
     backend_id: AccountId,
 }
 
 impl Default for TestingEnvBuilder {
-    fn default() -> TestingEnvBuilder {
-        todo!()
+    fn default() -> Self {
+        let alice = get_alice();
+
+        Self {
+            total_supply: (3_000_000_000 * ONE_LIS).into(),
+            constant_fee: ONE_LIS.into(),
+            percent_fee: 10,
+            owner_id: alice.id().clone(),
+            beneficiary_id: alice.id().clone(),
+            backend_id: alice.id().clone(),
+        }
     }
 }
 
 impl TestingEnvBuilder {
     pub async fn build(self) -> (Contract, Worker<Testnet>) {
-        todo!()
+        let worker = workspaces::testnet()
+            .await
+            .expect("Fail connect to testnet");
+        let wasm = std::fs::read(WASM_FILE).expect("No wasm file found");
+        let contract = worker
+            .dev_deploy(&wasm)
+            .await
+            .expect("Fail to deploy contract");
+        contract
+            .call(&worker, "new")
+            .args_json(&serde_json::to_value(&self).expect("Fail to serialize input"))
+            .expect("Invalid input args")
+            .transact()
+            .await
+            .expect("Fail to init contract");
+
+        (contract, worker)
     }
 
     pub fn set_total_supply(mut self, amount: u128) -> Self {
@@ -50,6 +80,11 @@ impl TestingEnvBuilder {
 
     pub fn set_percent_fee(mut self, amount: u8) -> Self {
         self.percent_fee = amount;
+        self
+    }
+
+    pub fn set_owner(mut self, account_id: AccountId) -> Self {
+        self.owner_id = account_id;
         self
     }
 
