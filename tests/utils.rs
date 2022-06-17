049bd6f8c83b71414e6bd63594795b3c0e7f4114
HolyGrease <kireevdanil2000@gmail.com>
2022-06-17 17:42:18 +0300

    make fmt

--- tests/utils.rs
+++ tests/utils.rs
@@ -1,30 +1,26 @@
-use near_sdk::AccountId;
-use near_sdk::serde::Serialize;
-use near_sdk::json_types::U128;
-pub use workspaces::{
-    network::Testnet, Contract, Worker, Account
-};
+use near_sdk::{json_types::U128, serde::Serialize, AccountId};
+pub use workspaces::{network::Testnet, Account, Contract, Worker};
 
 pub fn get_alice() -> Account {
-	todo!()
+    todo!()
 }
 
 pub fn get_bob() -> Account {
-	todo!()
+    todo!()
 }
 
 pub fn get_charlie() -> Account {
-	todo!()
+    todo!()
 }
 
 pub fn get_dave() -> Account {
-	todo!()
+    todo!()
 }
 
 #[derive(Serialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct TestingEnvBuilder {
-	total_supply: U128,
+    total_supply: U128,
     constant_fee: U128,
     percent_fee: u8,
     beneficiary_id: AccountId,
@@ -32,19 +28,19 @@ pub struct TestingEnvBuilder {
 }
 
 impl Default for TestingEnvBuilder {
-	fn default() -> TestingEnvBuilder {
-		todo!()
-	}
+    fn default() -> TestingEnvBuilder {
+        todo!()
+    }
 }
 
 impl TestingEnvBuilder {
-	pub async fn build(self) -> (Contract, Worker<Testnet>) {
-		todo!()
-	}
+    pub async fn build(self) -> (Contract, Worker<Testnet>) {
+        todo!()
+    }
 
-	pub fn set_constant_fee(&mut self, amount: u128) -> Self {
-		todo!()
-	}
+    pub fn set_constant_fee(&mut self, amount: u128) -> Self {
+        todo!()
+    }
 
-	// TODO: other setters
+    // TODO: other setters
 }
