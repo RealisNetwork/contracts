61f7e51e7420afea3858015415deb33cfa350b74
HolyGrease <kireevdanil2000@gmail.com>
2022-06-18 13:32:10 +0300

    make fmt

--- tests/mint.rs
+++ tests/mint.rs
@@ -1,23 +1,23 @@
 #[tokio::test]
 #[ignore]
 async fn mint_not_by_owner() {
-	// Setup contract: Alice - owner
+    // Setup contract: Alice - owner
 
-	// Bob call mint
+    // Bob call mint
 
-	// Assert error
+    // Assert error
 }
 
 #[tokio::test]
 #[ignore]
 async fn mint() {
-	// Setup contract: Alice - owner
+    // Setup contract: Alice - owner
 
-	// Alice mint for Bob
-	// Assert Bob has NFT
+    // Alice mint for Bob
+    // Assert Bob has NFT
 
-	// Alice mint for Charlie
-	// Alice mint for Charlie
-	// Assert Charlie has 2 NFTs
-	// Assert Bob has NFT 
+    // Alice mint for Charlie
+    // Alice mint for Charlie
+    // Assert Charlie has 2 NFTs
+    // Assert Bob has NFT
 }
