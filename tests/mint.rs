3410abec3bef1380bb9ed31345d8ee56c5533ee5
HolyGrease <kireevdanil2000@gmail.com>
2022-06-18 11:43:23 +0300

    tests draft for mint

---
+++ tests/mint.rs
@@ -0,0 +1,23 @@
+#[tokio::test]
+#[ignore]
+async fn mint_not_by_owner() {
+	// Setup contract: Alice - owner
+
+	// Bob call mint
+
+	// Assert error
+}
+
+#[tokio::test]
+#[ignore]
+async fn mint() {
+	// Setup contract: Alice - owner
+
+	// Alice mint for Bob
+	// Assert Bob has NFT
+
+	// Alice mint for Charlie
+	// Alice mint for Charlie
+	// Assert Charlie has 2 NFTs
+	// Assert Bob has NFT 
+}
