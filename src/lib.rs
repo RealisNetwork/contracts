fbbfab2bcd6a02bbb4ef9adf6f02776c5e18b9c5
gleb protasov <protasovgleb290@gmail.com>
2022-06-20 09:02:39 +0300

    fix after review

--- src/lib.rs
+++ src/lib.rs
@@ -106,22 +106,6 @@ impl Contract {
         }
     }
 
-    #[private]
-    #[init(ignore_state)]
-    pub fn migrate() -> Self {
-        let old_contract: Contract = env::state_read().expect("failed");
-        Self {
-            constant_fee: old_contract.constant_fee,
-            percent_fee: old_contract.percent_fee,
-            accounts: old_contract.accounts,
-            nfts: old_contract.nfts,
-            owner_id: old_contract.owner_id,
-            backend_id: old_contract.backend_id,
-            beneficiary_id: old_contract.beneficiary_id,
-            state: old_contract.state,
-            registered_accounts: old_contract.registered_accounts,
-        }
-    }
 
     pub fn lockups_info(
         &self,
