2635b647fb926d3b09725f65c18ecd78a12fc678
BeginArtem <lartem.1205@gmail.com>
2022-06-21 14:43:43 +0300

    add: don't take fee in backend

--- src/backend_api.rs
+++ src/backend_api.rs
@@ -24,13 +24,13 @@ impl Contract {
         // sender_free.into()
     }
 
-    pub fn backend_transfer_nft(&mut self, recipient_id: AccountId, nft_id: U128) -> U128 {
+    pub fn backend_transfer_nft(&mut self, recipient_id: AccountId, nft_id: U128) {
         self.assert_running();
         self.assert_backend();
         let sender_id = self.resolve_account(env::signer_account_pk());
         // let sender_free = self.take_fee(sender_id.clone(), None, true);
         self.internal_transfer_nft(sender_id, recipient_id, nft_id.0);
-        sender_free.into()
+        // sender_free.into()
     }
 
     #[allow(unused_variables)]
