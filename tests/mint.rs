fdda5b951a6af33079d701a27cdea8b5440ed840
RTahiiev <ruslan.tahiiev@swanteams.com>
2022-06-22 12:49:08 +0300

    Changes by glebprotasov

--- tests/mint.rs
+++ tests/mint.rs
@@ -1,21 +1,35 @@
+use crate::utils::*;
+
+mod utils;
+
 #[tokio::test]
-#[ignore]
+#[should_panic(expected = "Mint nft, Fail to make transaction.: \
+Action #0: ExecutionError(\"Smart contract panicked: Only owner can do this\")")]
 async fn mint_not_by_owner() {
     // Setup contract: Alice - owner
+    let bob = get_bob();
+    let (contract, worker) = TestingEnvBuilder::default().build().await;
 
     // Bob call mint
+    let result = test_call_mint_nft(&contract, &worker, &bob, &bob).await;
 
     // Assert error
 }
 
 #[tokio::test]
-#[ignore]
 async fn mint() {
     // Setup contract: Alice - owner
+    let alice = get_alice();
+    let charlie = get_charlie();
+    let bob = get_bob();
+
+    let (contract, worker) = TestingEnvBuilder::default().build().await;
 
     // Alice mint for Bob
+    test_call_mint_nft(&contract, &worker, &bob, &alice).await;
     // Assert Bob has NFT
-
+    let bob_info = test_call_get_acc_info(&bob, &worker, &contract).await;
+    assert!(bob_info.nfts.get(0).is_some());
     // Alice mint for Charlie
     // Alice mint for Charlie
     // Assert Charlie has 2 NFTs
