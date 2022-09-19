7f35591201d7d95b7aeb5ec8e8d5c423c50393a5
HolyGrease <kireevdanil2000@gmail.com>
2022-09-19 09:28:38 +0300

    transfer tokens on unstake

--- ft-staking-contract/src/lib.rs
+++ ft-staking-contract/src/lib.rs
@@ -1,9 +1,12 @@
+use near_contract_standards::fungible_token::receiver::ext_ft_receiver;
 use near_sdk::{
     borsh::{self, BorshDeserialize, BorshSerialize},
     collections::LookupMap,
-    env,
+    env, is_promise_success,
     json_types::U128,
-    near_bindgen, require, AccountId, Balance, PanicOnDefault, PromiseOrValue,
+    near_bindgen, require,
+    serde_json::json,
+    AccountId, Balance, Gas, PanicOnDefault, PromiseOrValue, Promise,
 };
 use xtoken::XTokenCost;
 
@@ -13,12 +16,24 @@ pub mod metadata;
 pub mod update;
 pub mod xtoken;
 
+pub const MILLISECOND: u64 = 1_000_000;
+pub const SECOND: u64 = 1000 * MILLISECOND;
+pub const MINUTE: u64 = 60 * SECOND;
+pub const HOUR: u64 = 60 * MINUTE;
+pub const DAY: u64 = 24 * HOUR;
+pub const WEEK: u64 = 7 * DAY;
+
+pub const GAS_FOR_UNSTAKE: Gas = Gas(20_000_000_000_000);
+pub const GAS_FOR_UNSTAKE_CALLBACK: Gas = Gas(20_000_000_000_000);
+
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
     pub owner_id: AccountId,
     /// Token contract account id to receive tokens for staking
     pub token_account_id: AccountId,
+    /// Lockup contract account id
+    pub lockup_account_id: AccountId,
     /// AccountID -> Account xtokens balance.
     accounts: LookupMap<AccountId, Balance>,
     /// Total supply of the all tokens in staking
@@ -32,10 +47,15 @@ pub struct Contract {
 #[near_bindgen]
 impl Contract {
     #[init]
-    pub fn new(owner_id: Option<AccountId>, token_account_id: AccountId) -> Self {
+    pub fn new(
+        owner_id: Option<AccountId>,
+        token_account_id: AccountId,
+        lockup_account_id: AccountId,
+    ) -> Self {
         Self {
             owner_id: owner_id.unwrap_or_else(env::predecessor_account_id),
             token_account_id,
+            lockup_account_id,
             accounts: LookupMap::new(b"a"),
             total_supply: 0,
             total_xtoken_supply: 0,
@@ -43,7 +63,7 @@ impl Contract {
         }
     }
 
-    pub fn unstake(&mut self, xtoken_amount: Option<U128>) {
+    pub fn unstake(&mut self, xtoken_amount: Option<U128>) -> Promise {
         let xtoken_amount = xtoken_amount.map(|a| a.0).unwrap_or_else(|| {
             self.accounts
                 .get(&env::predecessor_account_id())
@@ -53,7 +73,16 @@ impl Contract {
             xtoken_amount > 0,
             "The xtoken_amount should be a positive number"
         );
-        self.unstake_internal(&env::predecessor_account_id(), xtoken_amount);
+        self.unstake_internal(&env::predecessor_account_id(), xtoken_amount)
+    }
+
+    #[private]
+    pub fn transfer_on_unstake_callback(&mut self, account_id: AccountId, amount: U128) {
+        if is_promise_success() {
+            return;
+        }
+        // Rollback account stake
+        self.stake_internal(&account_id, amount.into());
     }
 }
 
@@ -74,7 +103,7 @@ impl Contract {
         .emit();
     }
 
-    pub fn unstake_internal(&mut self, account_id: &AccountId, xtoken_amount: Balance) {
+    pub fn unstake_internal(&mut self, account_id: &AccountId, xtoken_amount: Balance) -> Promise {
         let amount = self.xtoken_cost.convert_to_amount(xtoken_amount);
         self.total_supply -= amount;
         self.total_xtoken_supply -= xtoken_amount;
@@ -88,6 +117,23 @@ impl Contract {
             memo: None,
         }
         .emit();
+
+        ext_ft_receiver::ext(self.lockup_account_id.clone())
+            .with_static_gas(env::prepaid_gas() - GAS_FOR_UNSTAKE)
+            .ft_on_transfer(
+                env::current_account_id(),
+                amount.into(),
+                json!({
+                    "duration": format!("{}", WEEK),
+                    "account_id": account_id,
+                })
+                .to_string(),
+            )
+            .then(
+                Self::ext(env::current_account_id())
+                    .with_static_gas(GAS_FOR_UNSTAKE_CALLBACK)
+                    .transfer_on_unstake_callback(account_id.clone(), amount.into()),
+            )
     }
 
     pub fn add_to_pool_internal(&mut self, amount: Balance) {
