00d8ed02abeaa93399ce277714b45a03111a1cc5
HolyGrease <kireevdanil2000@gmail.com>
2022-09-19 13:58:47 +0300

    clean up

--- ft-lockup-contract/src/update.rs
+++ ft-lockup-contract/src/update.rs
@@ -15,32 +15,7 @@ impl Ownable for Contract {
 #[near_bindgen]
 impl Contract {
     #[init(ignore_state)]
-    pub fn update(owner_id: Option<AccountId>) -> Self {
-        #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
-        pub struct OldContract {
-            pub owner_id: AccountId,
-            /// Token contract account id to receive tokens for lockup
-            pub token_account_id: AccountId,
-            /// Account IDs that can create new lockups.
-            pub deposit_whitelist: UnorderedSet<AccountId>,
-            /// All lockups
-            pub lockups: UnorderedMap<LockupIndex, Lockup>,
-            /// Lockups indexes by AccountId
-            pub account_lockups: LookupMap<AccountId, HashSet<LockupIndex>>,
-
-            pub index: LockupIndex,
-        }
-
-        let contract: OldContract =
-            env::state_read().unwrap_or_else(|| env::panic_str("Not initialized"));
-
-        Self {
-            owner_id: owner_id.unwrap_or_else(env::predecessor_account_id),
-            token_account_id: contract.token_account_id,
-            deposit_whitelist: contract.deposit_whitelist,
-            lockups: contract.lockups,
-            account_lockups: contract.account_lockups,
-            index: contract.index,
-        }
+    pub fn update() -> Self {
+        env::state_read().unwrap_or_else(|| env::panic_str("Not initialized"))
     }
 }
