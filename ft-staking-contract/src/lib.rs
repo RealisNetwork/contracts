2b4ab24c92c275b20605898c78808d5a671a223d
HolyGrease <kireevdanil2000@gmail.com>
2022-11-10 11:45:21 +0200

    check for unstake callback

--- ft-staking-contract/src/lib.rs
+++ ft-staking-contract/src/lib.rs
@@ -97,10 +97,14 @@ impl Contract {
         account_id: AccountId,
         amount: U128,
         #[callback_result] used: Result<U128, PromiseError>,
-    ) {
-        let amount = amount.0 - used.map(|v| v.0).unwrap_or_default();
+        ) {
+        let amount = amount.0.checked_sub(
+                used.map(|v| v.0).unwrap_or_default()
+        ).unwrap_or_else(|| env::panic_str("Overflow occured"));
         // Rollback account stake
-        self.stake_internal(&account_id, amount);
+        if amount > 0 {
+            self.stake_internal(&account_id, amount);
+        }
     }
 }
 
