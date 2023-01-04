4e6e5525abaa42585970119ab3c4b9a8c9bc77de
HolyGrease <kireevdanil2000@gmail.com>
2023-01-04 15:32:15 +0200

    test claim for other account

--- ft-lockup-contract/tests/claim.rs
+++ ft-lockup-contract/tests/claim.rs
@@ -82,6 +82,106 @@ async fn claim() -> anyhow::Result<()> {
 }
 
 #[tokio::test]
+async fn claim_for_other_account() -> anyhow::Result<()> {
+    let worker = workspaces::sandbox().await?;
+    let sandbox = SandboxEnvironment::new(&worker).await?;
+    let amount = 100 * LIS;
+    let user = sandbox
+        .owner
+        .create_subaccount("user")
+        .initial_balance(10 * NEAR)
+        .transact()
+        .await?
+        .into_result()?;
+    sandbox.owner
+        .call(sandbox.token.id(), "storage_deposit")
+        .deposit(NEAR)
+        .args_json(serde_json::json!({
+            "account_id": user.id()
+        }))
+        .transact()
+        .await?
+        .into_result()?;
+
+    // Only staking contract can create lockups
+    // so transfer funds to staking contract
+    sandbox
+        .owner
+        .call(sandbox.token.id(), "ft_transfer")
+        .deposit(YOCTO)
+        .args_json(serde_json::json!({
+            "receiver_id": sandbox.staking.id(),
+            "amount": amount.to_string(),
+        }))
+        .transact()
+        .await?
+        .into_result()?;
+
+    // Create lock
+    sandbox
+        .staking
+        .as_account()
+        .call(sandbox.token.id(), "ft_transfer_call")
+        .deposit(YOCTO)
+        .gas(300000000000000)
+        .args_json(serde_json::json!({
+            "receiver_id": sandbox.lockup.id(),
+            "amount": amount.to_string(),
+            "msg": serde_json::json!({
+                "duration": SECOND,
+                "account_id": user.id(),
+            }).to_string()
+        }))
+        .transact()
+        .await?
+        .into_result()?;
+
+    worker.fast_forward(100).await?;
+
+    let lockups: HashMap<u32, serde_json::Value> = sandbox
+        .owner
+        .view(
+            sandbox.lockup.id(),
+            "get_account_lockups",
+            serde_json::json!({
+                "account_id": user.id(),
+            })
+            .to_string()
+            .into_bytes(),
+        )
+        .await?
+        .json()?;
+    let lockup_index = lockups.keys().next().expect("No lockups created!");
+
+    assert_eq!(
+        3_000_000_000_u128 * LIS - amount,
+        token::ft_balance_of(&sandbox.token, sandbox.owner.id()).await?
+    );
+    sandbox
+        .owner
+        .call(sandbox.lockup.id(), "claim")
+        .deposit(YOCTO)
+        .gas(300000000000000)
+        .args_json(serde_json::json!({
+            "index":lockup_index,
+            "account_id": user.id(),
+        }))
+        .transact()
+        .await?
+        .into_result()?;
+    assert_eq!(
+        amount,
+        token::ft_balance_of(&sandbox.token, user.id()).await?
+    );
+    assert_eq!(
+        3_000_000_000_u128 * LIS - amount,
+        token::ft_balance_of(&sandbox.token, sandbox.owner.id()).await?
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
 async fn claim_error_transfer_tokens() -> anyhow::Result<()> {
     let worker = workspaces::sandbox().await?;
     let sandbox = SandboxEnvironment::new(&worker).await?;
