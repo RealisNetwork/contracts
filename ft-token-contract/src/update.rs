4fdf52575dfbf92fd08c2b0111cc26f156cd3835
BeginArtem <lartem.1205@gmail.com>
2023-02-10 13:29:22 +0200

    Revert "add migration for new contract version"
    This reverts commit 5534085c62e3b3cf96211cc9fbab9ef6e526dab4.

--- ft-token-contract/src/update.rs
+++ ft-token-contract/src/update.rs
@@ -1,15 +1,5 @@
 use crate::*;
 
-// pub struct Contract {
-//     pub owner_id: AccountId,
-//     pub staking_contract: AccountId,
-//     pub lockup_contract: AccountId,
-//     pub ft: FungibleToken,
-//     pub last_mint: Timestamp,
-//     pub backend: UnorderedSet<AccountId>,
-//     pub suspensioned_accounts: UnorderedSet<AccountId>,
-// }
-
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct ContractV010 {
     pub owner_id: AccountId,
@@ -23,18 +13,8 @@ pub struct ContractV010 {
 impl Contract {
     #[private]
     #[init(ignore_state)]
-    pub fn update(lockup_id: AccountId, suspensioned_accounts: Vec<AccountId>) -> Self {
-        let contract_old: ContractV010 = env::state_read().unwrap();
-        let mut this = Self {
-            owner_id: contract_old.owner_id,
-            staking_contract: contract_old.staking_contract,
-            lockup_contract: lockup_id,
-            ft: contract_old.ft,
-            last_mint: contract_old.last_mint,
-            backend: contract_old.backend,
-            suspensioned_accounts: UnorderedSet::new(b"c".to_vec()),
-        };
-        this.suspensioned_accounts.extend(suspensioned_accounts);
-        this
+    pub fn update() -> Self {
+        // TODO: migration
+        todo!()
     }
 }
